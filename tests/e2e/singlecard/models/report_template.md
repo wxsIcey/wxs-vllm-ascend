6d1bd66b9cf4d8378c03cfcade12ed95968ece8b
Icey <1790571317@qq.com>
2025-07-28 09:59:41 +0000

    fix
    Signed-off-by: Icey <1790571317@qq.com>

--- tests/e2e/singlecard/models/report_template.md
+++ tests/e2e/singlecard/models/report_template.md
@@ -1,6 +1,6 @@
 # {{ model_name }}
 
-**vLLM Version**: vLLM: {{ vllm_version }} ([{{ vllm_commit[:7] }}](https://github.com/vllm-project/vllm/commit/{{ vllm_commit }}))
+**vLLM Version**: vLLM: {{ vllm_version }} ([{{ vllm_commit[:7] }}](https://github.com/vllm-project/vllm/commit/{{ vllm_commit }})),
 **vLLM Ascend Version**: {{ vllm_ascend_version }} ([{{ vllm_ascend_commit[:7] }}](https://github.com/vllm-project/vllm-ascend/commit/{{ vllm_ascend_commit }}))  
 **Software Environment**: CANN: {{ cann_version }}, PyTorch: {{ torch_version }}, torch-npu: {{ torch_npu_version }}  
 **Hardware Environment**: Atlas A2 Series  
@@ -10,13 +10,14 @@
 
 **Command**:  
 ```bash
-export MODEL_ARGS= {{ model_args }}
+export MODEL_ARGS={{ model_args }}
 lm_eval --model {{ model_type }} --model_args $MODEL_ARGS --tasks {{ datasets }} \ 
---num_fewshot {{ num_fewshot }} --limit {{ limit }} --batch_size {{ batch_size}}
+--apply_chat_template --fewshot_as_multiturn {% if num_fewshot is defined and num_fewshot != "N/A" %} --num_fewshot {{ num_fewshot }} {% endif %} \
+--limit {{ limit }} --batch_size {{ batch_size}}
 ```
 
-| Task                  | Filter           | n-shot | Metric      | Value     | Stderr |
-|-----------------------|-----------------:|-------:|-------------|----------:|-------:|
+| Task                  | Metric      | Value     | Stderr |
+|-----------------------|-------------|----------:|-------:|
 {% for row in rows -%}
-| {{ row.task.ljust(23) }} | {{ row.filter.rjust(15) }} | {{ row.n_shot | string.rjust(6) }} | {{ row.value }} | ± {{ "%.4f" | format(row.stderr | float) }} |
+| {{ row.task.ljust(23) }} | {{ row.metric.rjust(15) }} |{{ row.value }} | ± {{ "%.4f" | format(row.stderr | float) }} |
 {% endfor %}
--- tests/e2e/singlecard/models/test_lm_eval_correctness.py
+++ tests/e2e/singlecard/models/test_lm_eval_correctness.py
@@ -5,7 +5,7 @@ import numpy as np
 from jinja2 import Environment, FileSystemLoader
 import lm_eval
 
-RTOL = 0.03
+RTOL = 0.02
 
 
 @dataclass
@@ -31,42 +31,48 @@ def env_config():
         torch_npu_version=os.getenv('TORCH_NPU_VERSION')
     )
     
-    
-# Qwen/Qwen3-30B-A3B有一个模型参数为enable_expert_parallel=True
-# Qwen/Qwen2.5-VL-7B-Instruct"有一个模型参数为max_images=2
-# Qwen2.5-VL-7B-Instruct的执行命令不包含--num_fewshot
-# 这些参数像是为了达到好的结果做的调整，暂不考虑?
+
 def build_model_args(eval_config, tp_size):
     trust_remote_code = eval_config.get("trust_remote_code", False)
     max_model_len = eval_config.get("max_model_len", 4096)
-    return (
-        f"pretrained={eval_config['model_name']},"
-        f"tensor_parallel_size={tp_size},"
-        f"enforce_eager=true,"
-        f"add_bos_token=true,"
-        f"trust_remote_code={trust_remote_code},"
-        f"max_model_len={max_model_len}"
-    )
-
-
-def launch_lm_eval(eval_config, tp_size):
+    model_args = {
+        "pretrained": eval_config["model_name"],
+        "tensor_parallel_size": tp_size,
+        "enforce_eager": True,
+        "add_bos_token": True,
+        "trust_remote_code": trust_remote_code,
+        "max_model_len": max_model_len,
+    }
+    for s in ["max_images"]:
+        val = eval_config.get(s, None)
+        if val:
+            model_args[s] = val
+    return model_args
+    
+    
+def build_eval_args(eval_config, tp_size):
     model_args = build_model_args(eval_config, tp_size)
-    results = lm_eval.simple_evaluate(
-        model=eval_config.get("model", "vllm"),
-        model_args=model_args,
-        tasks=[task["name"] for task in eval_config["tasks"]],
-        num_fewshot=eval_config["num_fewshot"],
-        limit=eval_config["limit"],
-        batch_size="auto",
-    )
-    return model_args, results
+    eval_params = {
+        "model": eval_config.get("model", "vllm"),
+        "model_args": model_args,
+        "tasks": [task["name"] for task in eval_config["tasks"]],
+        "apply_chat_template": True,
+        "fewshot_as_multiturn": True,
+        "limit": eval_config.get("limit", None),
+        "batch_size": "auto",
+    }
+    
+    for s in ["num_fewshot"]:
+        eval_params[s] = eval_config.get(s, "N/A")
+    return eval_params
 
 
-def generate_report(tp_size, eval_config, report_data, report_template, output_path, env_config: EnvConfig):
+def generate_report(tp_size, eval_config, report_data, report_template, output_path, env_config):
     env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template(report_template)
+    template = env.get_template(str(report_template))
     model_args = build_model_args(eval_config, tp_size)
-    datasets = ", ".join([task["name"] for task in eval_config["tasks"]])
+    eval_params = build_eval_args(eval_config, tp_size)
+    
     report_content = template.render(
         vllm_version=env_config.vllm_version,
         vllm_commit=env_config.vllm_commit,
@@ -76,40 +82,39 @@ def generate_report(tp_size, eval_config, report_data, report_template, output_p
         torch_version=env_config.torch_version,
         torch_npu_version=env_config.torch_npu_version,
         model_name=eval_config["model_name"],
-        model_args=model_args,
-        model_type=eval_config["model"],
-        datasets=datasets,
-        num_fewshot=eval_config["num_fewshot"],
-        limit=eval_config["limit"],
-        batch_size=eval_config["batch_size"],
-        rows=report_data.rows
+        model_args=f"'{','.join(f'{k}={v}' for k, v in model_args.items())}'",
+        model_type=eval_params["model"],
+        datasets = ",".join(eval_params["tasks"]),
+        limit=eval_params["limit"],
+        batch_size="auto",
+        num_fewshot=eval_params["num_fewshot"],
+        rows=report_data["rows"]
     )
     with open(output_path, 'w', encoding='utf-8') as f:
         f.write(report_content)
         
 
-def test_lm_eval_correctness_param(config_filename, tp_size, report_template, output_path, env_config: EnvConfig):
+def lm_eval_correctness_param(config_filename, tp_size, report_template, output_path, env_config):
     eval_config = yaml.safe_load(config_filename.read_text(encoding="utf-8"))
-    results = launch_lm_eval(eval_config, tp_size)
-    success = True
+    eval_params = build_eval_args(eval_config, tp_size)
+    results = lm_eval.simple_evaluate(**eval_params)
+    is_success = True
     report_data = {"rows": []}
-    
+
     for task in eval_config["tasks"]:
         for metric in task["metrics"]:
             ground_truth = metric["value"]
             measured_value = results["results"][task["name"]][metric["name"]]
             print(f"{task['name']} | {metric['name']}: "
                   f"ground_truth={ground_truth} | measured={measured_value}")
-            success = success and np.isclose(
+            is_success = is_success and np.isclose(
                 ground_truth, measured_value, rtol=RTOL)
 
-            report_data.rows.append({
+            report_data["rows"].append({
                 "task": task["name"],
                 "metric": metric["name"],
-                "n_shot": eval_config["num_fewshot"],
                 "value": f"✅{measured_value}" if is_success else f"❌{measured_value}",
                 "stderr": results["results"][task["name"]].get("stderr", 0.0)
             })
-    generate_report(tp_size, eval_config, report_data, report_template, output_path, env_config: EnvConfig)
-    assert success
-    
+    generate_report(tp_size, eval_config, report_data, report_template, output_path, env_config)
+    assert is_success
